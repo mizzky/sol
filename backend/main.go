f779aa5e66711da6fa03ece095315bf67809f93f
mizzky <ein.dreieck12@gmail.com>
2026-03-27 10:17:44 +0000

    feat: add cors header

--- backend/main.go
+++ backend/main.go
@@ -31,7 +31,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"http://localhost:3000"},
 		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
 	}))
 
 	//4. ルーティング設定
