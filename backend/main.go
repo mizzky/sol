80ab28fb9fb6cacb6e3fcfdfcd546ab58c4c6c35
mizzky <ein.dreieck12@gmail.com>
2026-03-30 08:11:31 +0000

    feat(backend): update CORS AllowMethods

--- backend/main.go
+++ backend/main.go
@@ -30,7 +30,7 @@ func main() {
 	// CORS設定:Next.jsだけに絞る
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"http://localhost:3000"},
-		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
 	}))
 
