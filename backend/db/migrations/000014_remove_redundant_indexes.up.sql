2ddce75dd428c6f93948358d99bc9197bbc19230
mizzky <ein.dreieck12@gmail.com>
2026-08-23 21:55:33 +0000

    perf(db): remove redundant indexes

---
+++ backend/db/migrations/000014_remove_redundant_indexes.up.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_users_email;
+DROP INDEX IF EXISTS idx_categories_name;
+DROP INDEX IF EXISTS idx_products_sku;
+DROP INDEX IF EXISTS idx_carts_user_id;
+DROP INDEX IF EXISTS idx_cart_items_cart_id;
